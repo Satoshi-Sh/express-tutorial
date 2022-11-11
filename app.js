3018dc0539e08a0a6ce919936a67d0e465585f99
Satoshi-Sh <satoss1108@gmail.com>
2022-11-10 19:53:10 -0600

    Added

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog')
 
+const compression = require("compression")
+
 var app = express();
 
 // view engine setup
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression())
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
